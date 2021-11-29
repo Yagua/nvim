a0420f2991af983231638778f46d2edfeea97c69
Yagua <yagua.db@gmail.com>
2021-11-29 12:16:07 -0500

    change some Telescope colors

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -15,5 +15,9 @@ nebulous.setup {
   custom_colors = {
     LineNr =       { fg = "#5BBBDA", bg = "NONE", style = "NONE" },
     CursorLineNr = { fg = "#DCC03A", bg = "NONE", style = "NONE" },
+    TelescopeResultsBorder = { fg = "#A5A6A9", bg = "NONE", style = "NONE" },
+    TelescopePreviewBorder = { fg = "#A5A6A9", bg = "NONE", style = "NONE" },
+    TelescopePromptBorder =  { fg = "#A5A6A9", bg = "NONE", style = "NONE" },
+    TelescopeBorder =        { fg = "#A5A6A9", bg = "NONE", style = "NONE" },
   }
 }
