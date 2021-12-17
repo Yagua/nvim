6aaafd23cb07eeb41103ec44602206a67fbbae47
Yagua <yagua.db@gmail.com>
2021-12-17 17:20:08 -0500

    some changes in nvim-tree module

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -51,25 +51,20 @@ require'nvim-tree'.setup {
       list = {
         { key = "<CR>",           cb = tree_cb("edit") },
         { key = "t",              cb = tree_cb("edit") }, --custom key
-        { key = "o",              cb = tree_cb("edit") },
-        { key = "<2-LeftMouse>",  cb = tree_cb("edit") },
-        { key = "<2-RightMouse>", cb = tree_cb("cd") },
         { key = "<C-]>",          cb = tree_cb("cd") },
         { key = "<C-v>",          cb = tree_cb("vsplit") },
-        { key = "<C-x>",          cb = tree_cb("split") },
         { key = "<C-s>",          cb = tree_cb("split") }, --custom key
         { key = "<C-t>",          cb = tree_cb("tabnew") },
         { key = "<",              cb = tree_cb("prev_sibling") },
         { key = ">",              cb = tree_cb("next_sibling") },
         { key = "<BS>",           cb = tree_cb("close_node") },
         { key = "h",              cb = tree_cb("close_node") }, -- custom key
-        { key = "<S-CR>",         cb = tree_cb("close_node") },
         { key = "<Tab>",          cb = tree_cb("preview") },
         { key = "I",              cb = tree_cb("toggle_ignored") },
-        { key = "H",              cb = tree_cb("toggle_dotfiles") },
+        { key = ".",              cb = tree_cb("toggle_dotfiles") },
         { key = "R",              cb = tree_cb("refresh") },
         { key = "a",              cb = tree_cb("create") },
-        { key = "d",              cb = tree_cb("remove") },
+        { key = "<S-D>",          cb = tree_cb("remove") },
         { key = "r",              cb = tree_cb("rename") },
         { key = "<C-r>",          cb = tree_cb("full_rename") },
         { key = "x",              cb = tree_cb("cut") },
