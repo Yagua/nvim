e45325dade401e7e437022b98e1427a15cfee72d
Yagua <yagua.db@gmail.com>
2022-07-22 13:45:38 -0500

    Update nvim-tree config

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,5 +1,4 @@
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-require'nvim-tree'.setup {
+require("nvim-tree").setup({
   disable_netrw       = true,
   hijack_netrw        = true,
   open_on_setup       = false,
@@ -12,77 +11,53 @@ require'nvim-tree'.setup {
       quit_on_open = true,
     },
   },
-  diagnostics = {
-    enable = false,
-    -- icons = {
-    --   hint = "",
-    --   info = "",
-    --   warning = "",
-    --   error = "",
-    -- }
-  },
-  update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  filters = {
-    dotfiles = true,
-    custom = {}
-  },
   view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = 'left',
     number = true,
     relativenumber = true,
-    signcolumn = "no",
+    adaptive_size = false,
     mappings = {
-      custom_only = false,
       list = {
-        { key = "<CR>",           cb = tree_cb("edit") },
-        { key = "t",              cb = tree_cb("edit") }, --custom key
-        { key = "<C-]>",          cb = tree_cb("cd") },
-        { key = "<C-v>",          cb = tree_cb("vsplit") },
-        { key = "<C-s>",          cb = tree_cb("split") }, --custom key
-        { key = "<C-t>",          cb = tree_cb("tabnew") },
-        { key = "<",              cb = tree_cb("prev_sibling") },
-        { key = ">",              cb = tree_cb("next_sibling") },
-        { key = "<BS>",           cb = tree_cb("close_node") },
-        { key = "h",              cb = tree_cb("close_node") }, -- custom key
-        { key = "<Tab>",          cb = tree_cb("preview") },
-        { key = "I",              cb = tree_cb("toggle_ignored") },
-        { key = ".",              cb = tree_cb("toggle_dotfiles") },
-        { key = "R",              cb = tree_cb("refresh") },
-        { key = "a",              cb = tree_cb("create") },
-        { key = "<S-D>",          cb = tree_cb("remove") },
-        { key = "r",              cb = tree_cb("rename") },
-        { key = "<C-r>",          cb = tree_cb("full_rename") },
-        { key = "x",              cb = tree_cb("cut") },
-        { key = "c",              cb = tree_cb("copy") },
-        { key = "p",              cb = tree_cb("paste") },
-        { key = "[c",             cb = tree_cb("prev_git_item") },
-        { key = "]c",             cb = tree_cb("next_git_item") },
-        { key = "-",              cb = tree_cb("dir_up") },
-        { key = "q",              cb = tree_cb("close") }
-      }
-    }
-  },
-  renderer = {
-    indent_markers = {
-      enable = true,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
+        { key = "<CR>",           action = "edit" },
+        { key = "l",              action = "edit" },
+        { key = "<C-]>",          action = "cd" },
+        { key = "<C-v>",          action = "vsplit" },
+        { key = "<C-s>",          action = "split" },
+        { key = "<C-t>",          action = "tabnew" },
+        { key = "<",              action = "prev_sibling" },
+        { key = ">",              action = "next_sibling" },
+        { key = "h",              action = "close_node" },
+        { key = "<Tab>",          action = "preview" },
+        { key = "I",              action = "toggle_ignored" },
+        { key = ".",              action = "toggle_dotfiles" },
+        { key = "R",              action = "refresh" },
+        { key = "a",              action = "create" },
+        { key = "<S-D>",          action = "remove" },
+        { key = "r",              action = "rename" },
+        { key = "<C-r>",          action = "full_rename" },
+        { key = "x",              action = "cut" },
+        { key = "c",              action = "copy" },
+        { key = "p",              action = "paste" },
+        { key = "[c",             action = "prev_git_item" },
+        { key = "]c",             action = "next_git_item" },
+        { key = "-",              action = "dir_up" },
+        { key = "q",              action = "close" }
       },
     },
   },
-}
+  renderer = {
+    group_empty = true,
+    indent_markers = {
+      enable = true,
+    },
+    icons = {
+      show = {
+        folder_arrow = false
+      }
+    }
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
 
 vim.api.nvim_set_keymap("n", "<c-n>", ":NvimTreeToggle<CR>", {noremap = true})
