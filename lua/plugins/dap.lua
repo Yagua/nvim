fd99fae010da36b711d61f4986d82cfc2f325069
Yagua <yagua.db@gmail.com>
2024-06-09 16:54:21 -0500

    feat: add go dap config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -132,6 +132,25 @@ local setup_adapters = function(dap)
 
   -- Python
   require('dap-python').setup(msn_path .. 'debugpy/venv/bin/python')
+  require('dap-go').setup {
+    dap_configurations = {
+      {
+        type = "go",
+        name = "Attach remote",
+        mode = "remote",
+        request = "attach",
+      },
+    },
+    delve = {
+      path = "dlv",
+      initialize_timeout_sec = 20,
+      port = "${port}",
+      args = {},
+      build_flags = "",
+      detached = true,
+      cwd = nil,
+    },
+  }
 end
 
 return {
