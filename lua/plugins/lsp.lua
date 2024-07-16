90658807b584f9d98ac78fc45dba33a39649edb0
Yagua <yagua.db@gmail.com>
2024-07-16 17:26:21 -0500

    feat: update nvim config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -390,4 +390,20 @@ return {
       jdtls.start_or_attach(config)
     end,
   },
+
+  -- go.nvim
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
 }
